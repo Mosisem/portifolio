9d693417cdaaef6a9e43851ead69255e8a6c9b80
Mosi sebastião <mosijoao4002@gmail.com>
2024-12-25 18:07:39 -0300

    mt

---
+++ js/projetos.js
@@ -0,0 +1,16 @@
+function rediret_0(){
+    window.open('https://mosisem.github.io/tcc/')
+}
+
+function rediret_1(){
+    window.open('https://mosisem.github.io/Helena_lombardin/')
+}
+
+function rediret_2(){
+    window.open('https://mosisem.github.io/net_games/')
+}
+
+
+function rediret_3(){
+    window.open('https://mosisem.github.io/mercado_livre/')
+}
