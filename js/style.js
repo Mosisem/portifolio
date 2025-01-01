cd3c05dabfecc00416c153f1f234b76125e0c428
Mosi sebastião <mosijoao4002@gmail.com>
2025-01-01 19:27:50 -0300

    jhthg

--- js/style.js
+++ js/style.js
@@ -1,6 +1,27 @@
-ScrollReveal().reveal('.box_projetos',{
+ScrollReveal().reveal('.projetos_',{
+    origin:`bottom`,
+    distance:'20%',
+    durarion:23000,
+    delay:685
+});
+
+ScrollReveal().reveal('.sention_apresentacao',{
+    origin:`bottom`,
+    distance:'20%',
+    durarion:24100,
+    delay:680
+});
+
+ScrollReveal().reveal('.sobre_mim',{
     origin:`left`,
     distance:'20%',
-    durarion:20000,
-    delay:660
+    durarion:23000,
+    delay:680
+});
+
+ScrollReveal().reveal('.habilidades',{
+    origin:`left`,
+    distance:'10%',
+    durarion:23000,
+    delay:680
 });
